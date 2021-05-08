b4f4eca02d7fed26bbbed432801e03f4cca169b8
Content Manager <arely.trujillo@quadit.mx>
2021-05-08 10:59:21 -0500

    Primer commit

---
+++ modulo_quadit/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Modulo Quadit",
+    "version": "14.0",
+    "depends": ["base"],
+    "author": (
+        
+        "Arely Trujillo"
+    ),
+    "license": "AGPL-3",
+    "summary": """Modulo de practica Quadit""",
+    "website": "www.quadit.mx",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
